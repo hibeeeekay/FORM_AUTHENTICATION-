1a81fb57fa8fcd192076a1882c22e8ea65ac56b4
hibeeeekay <60797412+hibeeeekay@users.noreply.github.com>
2021-03-07 01:20:36 +0100

    Update validate.php

--- FORM_AUTHENTICATION/validate.php
+++ FORM_AUTHENTICATION/validate.php
@@ -1,11 +1,17 @@
 <?php
-  $myemail = "hibeeee@gmail.com";
+  $myemail = "hibeeeekay@gmail.com";
   $mypass  = "choice";
 
   if(isset($_POST['login'])){
      $email = $_POST['email'];
      $pass = $_POST['password'];
-     if($email == $myemail and $pass == $mypass){
+         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  echo("$email is a valid email address");
+} else {
+  echo("$email is not a valid email address");
+}
+
+          if($email == $myemail and $pass == $mypass){
         if(isset($_POST['remember'])){
             setcookie('email', $email, time()+ 3600 * 24 );
         } 
